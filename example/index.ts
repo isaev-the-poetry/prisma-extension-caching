0def377cf0bd5d64eb6619f6de2e199ec3105f25
isaev <cutmaestro@gmail.com>
2023-06-25 21:00:48 +0700

    v1 Igor Isaev

--- example/index.ts
+++ example/index.ts
@@ -1,22 +1,29 @@
-import { PrismaClient } from "@prisma/client";
-import { existsFn } from "../dist"
+import { PrismaClient, Prisma } from "@prisma/client";
+import { caching } from "../dist"
 
-const prisma = new PrismaClient().$extends(existsFn({}))
+// You can use multiple clients. 
+// ex: const prisma = new PrismaClient()
+const cache = new PrismaClient().$extends(caching())
+// Caching is enabled always. 
 
 async function main() {
-  const user = await prisma.user.exists({ where: { id: 1 } })
 
-  const post = await prisma.post.exists({
-    where: {
-      OR: [
-        { title: { contains: 'prisma' } },
-        { content: { contains: 'prisma' } },
-      ],
-      published: true,
-    },
-  })
+  // First time: query will retrive data, and save it in local prisma.cache model
+  // Next time: data will be fetched from cache. No more database calls untill purge.
+  // Query arguments will be used as flexible caching key.
+  const recentPosts = await cache.post.findMany({ orderBy: { id: "desc" }, take: 10 })
+  
+  console.log({ recentPosts })
 
-  console.log({ user, post })
+  // You controll purging manually.
+  await cache.post.purge({ orderBy: { id: "desc" }, take: 10 }) // model query exact match
+  await cache.post.purge({ path: ['where', 'visible'], not: Prisma.DbNull }) // model query JsonFilter match 
+  await cache.post.purge({ hours: 3 }) // model any cache older than 3 hours
+  await cache.post.purge() // any model cache
+
+  // same purpose without caching
+  // ex: const prisma = new PrismaClient()
+  // const originalPosts = await prisma.post.findMany({ orderBy: { id: "desc" }, take: 10 })
 }
 
 main()
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
-import { Prisma } from '@prisma/client/extension'
+import { Prisma } from '@prisma/client'
 
-type Args = {}
+export type range = { hours: number }
 
-export const existsFn = (_extensionArgs: Args) =>
-  Prisma.defineExtension({
-    name: "prisma-extension-find-or-create",
-    model: {
-      $allModels: {
-        async exists<T, A>(
-          this: T,
-          args: Prisma.Exact<A, Prisma.Args<T, 'findFirst'>>
-        ): Promise<boolean> {
+const isRange = (args: {}): args is range => "hours" in args
+const isFilter = (args: {}): args is Prisma.JsonFilter => "path" in args
+const delayHours = (purgeDate: Date, hours: number): Date => (purgeDate.setTime(purgeDate.getTime() - (hours * 60 * 60 * 1000)), purgeDate)
 
-          const ctx = Prisma.getExtensionContext(this)
-          const result = await (ctx as any).findFirst(args)
-          return result !== null
+export const caching = () =>
+  Prisma.defineExtension((client) => {
+    return client.$extends({
+      name: 'prisma-extension-caching',
+      model: {
+        $allModels: {
+          async purge<T, A>(this: T, args?: Prisma.Args<T, 'findMany'> | Prisma.JsonFilter | range) {
+            const ctx = Prisma.getExtensionContext(this)
+            const model = ctx.name
+            const operation = 'findMany'
+
+            if (!args)
+              return client.cache.deleteMany({ where: { model, operation } })
+            else if (isFilter(args))
+              return client.cache.deleteMany({ where: { model, operation, key: args } })
+            else if (isRange(args))
+              return client.cache.deleteMany({ where: { created: { lt: delayHours(new Date(), args.hours) } } })
+            else if (model)
+              return client.cache.deleteMany({ where: { model, operation, key: { equals: args } } })
+          },
         },
       },
-    },
+      query: {
+        $allModels: {
+          async findMany({ model, operation, args, query }) {
+            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } } })
+            if (cache)
+              return cache.value
+
+            const result = await query(args)
+            await client.cache.upsert({
+              where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } },
+              create: { model, operation, key: args as Prisma.JsonObject, value: result as Prisma.JsonArray },
+              update: { value: result as Prisma.JsonArray }
+            })
+            return result
+          },
+        },
+      },
+    })
   })
