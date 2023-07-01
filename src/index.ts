e3d68ef4e291ea73c06426ed4148ad90c92de5c6
isaev <cutmaestro@gmail.com>
2023-07-01 23:26:25 +0300

    Refactoring:: createCache, getCache methods. More dry code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import { Prisma } from '@prisma/client'
+import { Args, DefaultArgs, DynamicClientExtensionThis } from '@prisma/client/runtime'
 
 type range = { hours: number } | { seconds: number }
 
 export const supportedMethod = ['findMany', 'groupBy', 'count', 'aggregate', 'findUnique', 'findFirst'] as const
 export type supportedMethod = typeof supportedMethod[number]
 
-const isRange = (args: {}): args is range => ("hours" in args) || "seconds" in args 
+const isRange = (args: {}): args is range => ("hours" in args) || "seconds" in args
 const isFilter = (args: {}): args is Prisma.JsonFilter => "path" in args
 const isMethod = (method: unknown): method is supportedMethod => supportedMethod.includes(method as supportedMethod)
 const delayHours = (purgeDate: Date, hours: number): Date => (purgeDate.setTime(purgeDate.getTime() - (hours * 60 * 60 * 1000)), purgeDate)
 const delaySeconds = (purgeDate: Date, seconds: number): Date => (purgeDate.setTime(purgeDate.getTime() - (seconds * 1000)), purgeDate)
+
+type CacheProps = {
+  args: {}
+  result: Prisma.JsonArray | Prisma.JsonObject | Prisma.CacheCountAggregateOutputType | Prisma.PostCountAggregateOutputType | number
+  operation: supportedMethod
+  model: Prisma.ModelName
+}
+
+type client = DynamicClientExtensionThis<Prisma.TypeMap<Args & DefaultArgs>, Prisma.TypeMapCb, DefaultArgs>
+
+
+const createCache = async (client: client, { model, args, operation, result }: CacheProps) => (await client.cache.upsert({
+  where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } },
+  create: { model, operation, key: args as unknown as Prisma.JsonObject, value: result as Prisma.JsonObject },
+  update: { value: result || {} as Prisma.JsonObject, updated: new Date() }
+})).value
+
+const getCache = async (client: client, { model, args, operation }: Omit<CacheProps, "result">) => client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } } })
 
 export const caching = () =>
   Prisma.defineExtension((client) => {
@@ -17,7 +36,7 @@ export const caching = () =>
       name: 'prisma-extension-caching',
       model: {
         $allModels: {
-          async purge<T, A>(this: T, args?: Prisma.Args<T, 'findMany'> | Prisma.JsonFilter | range | supportedMethod, operation?: supportedMethod) {
+          async purge<T, A>(this: T, args?: Prisma.Args<T, supportedMethod> | Prisma.JsonFilter | range | supportedMethod, operation?: supportedMethod) {
             const ctx = Prisma.getExtensionContext(this)
             const model = ctx.name
 
@@ -46,86 +65,37 @@ export const caching = () =>
       },
       query: {
         $allModels: {
-          /**
-           *  I have copy paste some methods to save type inference.
-           */
           async findMany({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } } })
-            if (cache)
-              return cache.value
-
-            const result = await query(args)
-            await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } },
-              create: { model, operation, key: args as Prisma.JsonObject, value: result as Prisma.JsonArray },
-              update: { value: result as Prisma.JsonArray, updated: new Date() }
-            })
-            return result
+            const cache = await getCache(client, { model, operation, args })
+            return cache ? cache.value : createCache(client, { model, args, operation, result: await query(args) })
           },
           async findUnique({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } } })
+            const cache = await getCache(client, { model, operation, args })
             if (cache)
               return cache.value
 
             const result = await query(args)
-            result && await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } },
-              create: { model, operation, key: args as unknown as Prisma.JsonObject, value: result as Prisma.JsonObject },
-              update: { value: result || {} as Prisma.JsonObject, updated: new Date() }
-            })
-            return result
+            return result && createCache(client, { model, args, operation, result })
           },
           async findFirst({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } } })
+            const cache = await getCache(client, { model, operation, args })
             if (cache)
               return cache.value
 
             const result = await query(args)
-            result && await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as unknown as Prisma.JsonObject } },
-              create: { model, operation, key: args as unknown as Prisma.JsonObject, value: result as Prisma.JsonObject },
-              update: { value: result || {} as Prisma.JsonObject, updated: new Date() }
-            })
-            return result
-          }, 
+            return result && createCache(client, { model, args, operation, result })
+          },
           async groupBy({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } } })
-            if (cache)
-              return cache.value
-
-            const result = await query(args)
-            await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } },
-              create: { model, operation, key: args as Prisma.JsonObject, value: result as Prisma.JsonArray },
-              update: { value: result as Prisma.JsonArray, updated: new Date() }
-            })
-            return result
+            const cache = await getCache(client, { model, operation, args })
+            return cache ? cache.value : createCache(client, { model, args, operation, result: await query(args) })
           },
           async count({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } } })
-            if (cache)
-              return (cache.value as { count: number })?.count
-
-            const result = await query(args)
-            await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } },
-              create: { model, operation, key: args as Prisma.JsonObject, value: { count: result } as Prisma.JsonObject },
-              update: { value: { count: result } as Prisma.JsonObject, updated: new Date() }
-            })
-            return result
+            const cache = await getCache(client, { model, operation, args })
+            return cache ? cache.value : createCache(client, { model, args, operation, result: await query(args) })
           },
           async aggregate({ model, operation, args, query }) {
-            const cache = await client.cache.findUnique({ where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } } })
-            if (cache)
-              return cache.value
-
-            const result = await query(args)
-            await client.cache.upsert({
-              where: { model_operation_key: { model, operation, key: args as Prisma.JsonObject } },
-              create: { model, operation, key: args as Prisma.JsonObject, value: result as Prisma.JsonArray },
-              update: { value: result as Prisma.JsonArray, updated: new Date() }
-            })
-            return result
+            const cache = await getCache(client, { model, operation, args })
+            return cache ? cache.value : createCache(client, { model, args, operation, result: await query(args) })
           },
         },
       },
